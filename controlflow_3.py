f3bb1c41a00ce236e4605ce59637e1cb4ddb3bc9
elias <elias.papachristos@gmail.com>
2019-05-20 10:56:09 +0300

    Initial commit (^_^)

---
+++ controlflow_3.py
@@ -0,0 +1,90 @@
+"""
+Zip coordinates
+"""
+x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
+y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
+z_coord = [4, 16, -6, -42, 3, -6, 23, -1]
+labels = ["F", "J", "A", "Q", "Y", "B", "W", "X"]
+
+points = []
+# write your for loop here
+for point in zip(labels, x_coord, y_coord, z_coord):
+    points.append('{}: {}, {}, {}'.format(*point))
+
+for point in points:
+    print(point)
+
+
+"""
+Zip Lists to a Dictionary
+"""
+cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
+cast_heights = [72, 68, 72, 66, 76]
+
+cast = dict(zip(cast_names, cast_heights))
+print(cast)
+
+
+"""
+Unzip Tuples
+"""
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+# define names and heights here
+names, heights = zip(*cast)
+
+print(names)
+print(heights)
+
+
+"""
+Transpose with Zip
+"""
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+data_transpose = tuple(zip(*data))
+print(data_transpose)
+
+
+"""
+Enumerate
+"""
+cast = ["Barney Stinson", "Robin Scherbatsky", "Ted Mosby", "Lily Aldrin", "Marshall Eriksen"]
+heights = [72, 68, 72, 66, 76]
+
+# write your for loop here
+for i, actor in enumerate(cast):
+    cast[i] = actor + ' ' + str(heights[i])
+
+print(cast)
+
+
+"""
+Extract First Names
+"""
+names = ["Rick Sanchez", "Morty Smith", "Summer Smith", "Jerry Smith", "Beth Smith"]
+
+first_names = [name.split()[0].lower() for name in names]
+print(first_names)
+
+
+"""
+Multiples of 3
+"""
+multiples_3 = [x * 3 for x in range(1, 21)]
+print(multiples_3)
+
+
+"""
+Filter Names by Scores
+"""
+scores = {
+             "Rick Sanchez": 70,
+             "Morty Smith": 35,
+             "Summer Smith": 82,
+             "Jerry Smith": 23,
+             "Beth Smith": 98
+          }
+
+passed = [name for name, score in scores.items() if score >= 65]
+print(passed)
