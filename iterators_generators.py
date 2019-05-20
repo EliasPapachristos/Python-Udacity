f6c065d2e6650224c330f5f5e85218219fd8f787
elias <elias.papachristos@gmail.com>
2019-05-20 10:19:17 +0300

    This is my code for Iterators and Generators

--- iterators_generators.py
+++ iterators_generators.py
@@ -27,3 +27,15 @@ def chunker(iterable, size):
 
 for chunk in chunker(range(25), 4):
     print(list(chunk))
+
+
+"""
+Generator Expressions
+Here's a cool concept that combines generators and list comprehensions! 
+You can actually create a generator in the same way you'd normally write a list comprehension, 
+except with parentheses instead of square brackets.
+"""
+
+sq_list = [x**2 for x in range(10)]  # this produces a list of squares
+
+sq_iterator = (x**2 for x in range(10))  # this produces an iterator of squares
